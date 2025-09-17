487bbff1bba37e4a2a5dc2a09cfcef4283860cd3
coder7475 <robiulhossain7475@gmail.com>
2025-09-17 12:03:11 +0600

    feat(websocket): create websocket server

---
+++ websocket/server.js
@@ -0,0 +1,20 @@
+const WebSocket = require("ws");
+
+const wsServer = new WebSocket.Server({ port: 8080 });
+
+console.log("Web Socket Server running on http://localhost:8080");
+
+wsServer.on("connection", (ws) => {
+  console.log("New Client Connected!");
+
+  ws.send("Welcome to WebSocket Server!");
+
+  ws.on("message", (message) => {
+    console.log(`Received: ${message}`);
+    ws.send(`Server Received: ${message}`);
+  });
+
+  ws.on("close", () => {
+    console.log("Client Disconnected");
+  });
+});
